62aa99cce616b85a67000afbb6cbdda3b5dd1bb3
Jack Lindgren <jackmarkcharleslindgren@gmail.com>
2025-11-05 14:34:26 -0700

    Initialize project and install base dependencies.

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import netlify from '@netlify/vite-plugin';
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [reactRouter(), tailwindcss(), netlify()],
+});
